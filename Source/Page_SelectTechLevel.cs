f46c069892faca9b627a05dc80e318221759c396
Garwel <garwel@protonmail.com>
2022-10-17 17:58:17 +0200

    Can disable starting techs; show tooltips with # of techs

--- Source/Page_SelectTechLevel.cs
+++ Source/Page_SelectTechLevel.cs
@@ -1,4 +1,6 @@
 ﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Verse;
 
@@ -10,40 +12,61 @@ namespace StartingTechLevel
     {
         bool defaultTechLevel = true;
         TechLevel techLevel;
+        bool grantStartingTechs = true;
+        List<int> techsByLevel = new List<int>(7);
 
         public override string PageTitle => "Choose Tech Level";
 
-        void SetTechLevel(Faction faction, TechLevel techLevel)
+        void SetTechLevel(Faction faction)
         {
+            // Setting tech level
             TechLevel oldTechLevel = faction.def.techLevel;
             faction.def.techLevel = techLevel;
-            Find.ResearchManager.ResetAllProgress();
-
-            foreach (ResearchProjectDef researchProject in DefDatabase<ResearchProjectDef>.AllDefs)
-                if (researchProject.techLevel < techLevel)
-                {
-                    Log($"Finishing research project {researchProject} ({researchProject.LabelCap}).");
-                    Find.ResearchManager.FinishProject(researchProject, doCompletionLetter: false);
-                }
-
             Log($"Player faction {faction.Name} changed tech level from {oldTechLevel.ToStringSafe()} to {faction.def.techLevel.ToStringSafe()}");
+
+            // Researching techs from previous levels
+            foreach (ResearchProjectDef researchProject in DefDatabase<ResearchProjectDef>.AllDefs.Where(researchProject => researchProject.techLevel < techLevel))
+            {
+                Log($"Finishing research project {researchProject} ({researchProject.LabelCap}).");
+                Find.ResearchManager.FinishProject(researchProject, doCompletionLetter: false);
+            }
+
+            // Setting correct starting research tags
+            faction.def.startingResearchTags.Clear();
+            if (grantStartingTechs)
+                if (techLevel >= TechLevel.Industrial)
+                    faction.def.startingResearchTags.Add(ResearchProjectTagDefOf.ClassicStart);
+                else if (techLevel >= TechLevel.Neolithic)
+                    faction.def.startingResearchTags.Add(DefDatabase<ResearchProjectTagDef>.GetNamed("TribalStart"));
         }
 
         protected override void DoNext()
         {
-            Log($"DoNext (defaultTechLevel: {defaultTechLevel}; techLevel: {techLevel})");
             Faction playerFaction = Find.GameInitData.playerFaction;
             if (!defaultTechLevel && playerFaction != null)
-                SetTechLevel(playerFaction, techLevel);
+                SetTechLevel(playerFaction);
             base.DoNext();
         }
 
-        bool RadioButtonLabeled(ref Rect rect, string labelText, bool chosen)
+        public override void PreOpen()
+        {
+            int total = 0;
+            techsByLevel.Add(0);
+            for (TechLevel l = TechLevel.Neolithic; l <= TechLevel.Archotech; l++)
+            {
+                total += ResearchProjectUtility.ResearchProjectsAtTechLevel(l - 1).Count();
+                techsByLevel.Add(total);
+            }
+            base.PreOpen();
+        }
+
+        bool RadioButtonLabeled(ref Rect rect, string labelText, bool chosen, TipSignal tipSignal)
         {
             bool res = Widgets.RadioButton(rect.x, rect.y, chosen);
             float y = rect.y;
-            Widgets.Label(rect.x + Widgets.RadioButtonSize + 5, ref y, rect.width - Widgets.RadioButtonSize - 5, labelText);
-            rect.y = y + Widgets.RadioButtonSize + 5;
+            Widgets.Label(rect.x + Widgets.RadioButtonSize + 5, ref y, rect.width - Widgets.RadioButtonSize - 5, labelText, tipSignal);
+            rect.y += Widgets.RadioButtonSize + 15;
+            rect.height -= Widgets.RadioButtonSize + 15;
             return res;
         }
 
@@ -52,17 +75,23 @@ namespace StartingTechLevel
             DrawPageTitle(rect);
             Rect mainRect = GetMainRect(rect);
             Widgets.BeginGroup(mainRect);
-            if (RadioButtonLabeled(ref mainRect, "Default", defaultTechLevel))
+            Text.Anchor = TextAnchor.UpperLeft;
+
+            if (RadioButtonLabeled(ref mainRect, "Default", defaultTechLevel, "Vanilla start"))
             {
                 defaultTechLevel = true;
                 techLevel = TechLevel.Undefined;
             }
-            for (TechLevel l = TechLevel.Animal; l < TechLevel.Archotech; l++)
-                if (RadioButtonLabeled(ref mainRect, l.ToStringHuman().CapitalizeFirst(), techLevel == l))
+            for (TechLevel l = TechLevel.Animal; l <= TechLevel.Archotech; l++)
+                if (RadioButtonLabeled(ref mainRect, l.ToStringHuman().CapitalizeFirst(), techLevel == l, new TipSignal($"Start with {techsByLevel[(int)l - 1]} techs")))
                 {
                     defaultTechLevel = false;
                     techLevel = l;
                 }
+
+            if (techLevel == TechLevel.Neolithic || techLevel == TechLevel.Industrial)
+                Widgets.CheckboxLabeled(mainRect, "Grant starting technologies", ref grantStartingTechs);
+
             Widgets.EndGroup();
             DoBottomButtons(rect);
         }
---
+++ Source/ResearchProjectUtility.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace StartingTechLevel
+{
+    public static class ResearchProjectUtility
+    {
+        public static IEnumerable<ResearchProjectDef> ResearchProjectsAtTechLevel(TechLevel techLevel) =>
+            DefDatabase<ResearchProjectDef>.AllDefs.Where(researchProject => researchProject.techLevel == techLevel);
+    }
+}
