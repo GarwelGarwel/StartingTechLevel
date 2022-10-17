1c728834218a0e6a13357fe36b415ead5c3e99be
Garwel <garwel@protonmail.com>
2022-10-17 09:41:54 +0200

    Add page to new game generation

---
+++ Source/Page_SelectTechLevel.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+using static StartingTechLevel.LogUtility;
+
+namespace StartingTechLevel
+{
+    public class Page_SelectTechLevel : Page
+    {
+        public override string PageTitle => "Choose Tech Level";
+
+        public override void DoWindowContents(Rect rect)
+        {
+            DrawPageTitle(rect);
+            Widgets.BeginGroup(GetMainRect(rect));
+            Widgets.EndGroup();
+            DoBottomButtons(rect);
+        }
+    }
+}
--- Source/StartingTechLevel.cs
+++ Source/StartingTechLevel.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using RimWorld;
 using Verse;
 
 using static StartingTechLevel.LogUtility;
@@ -11,9 +12,38 @@ namespace StartingTechLevel
         static Harmony harmony;
 
         static StartingTechLevel()
-        { 
+        {
+            if (harmony != null)
+                return;
+
             harmony = new Harmony("Garwel.StartingTechLevel");
+            harmony.Patch(
+                AccessTools.Method($"RimWorld.Scenario:GetFirstConfigPage"),
+                postfix: new HarmonyMethod(typeof(StartingTechLevel).GetMethod("Scenario_GetFirstConfigPage")));
             Log("StartingTechLevel initialized successfully.", LogLevel.Important);
+        }
+
+        /// <summary>
+        /// Adds Select Tech Level page after the first one
+        /// </summary>
+        /// <param name="__result"></param>
+        public static void Scenario_GetFirstConfigPage(ref Page __result)
+        {
+            Log("Scenario_GetFirstConfigPage");
+            Page storytellerPage = __result;
+            while (storytellerPage != null && !(storytellerPage is Page_SelectStoryteller))
+                storytellerPage = storytellerPage.next;
+            if (storytellerPage == null)
+            {
+                Log($"Select Storyteller page not found. First page: {__result?.PageTitle}.", LogLevel.Important);
+                return;
+            }
+            Page_SelectTechLevel newPage = new Page_SelectTechLevel();
+            newPage.prev = storytellerPage;
+            newPage.next = storytellerPage.next;
+            storytellerPage.next = newPage;
+            newPage.next.prev = newPage;
+            Log("Select Tech Level page added.");
         }
     }
 }
